2aed463ca7955d43ab74563a6011dc2f001742d2
Ziggy Chen <ziggycs9796@hotmail.com>
2023-06-24 22:08:33 +0800

    feat: subapp 使用全局 store

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,30 +2,19 @@
   <div>
     <h1 class="text-blue mb-10">SubApp Home page</h1>
     <button @click="goBack">go back</button>
-    <!-- <h2>{{counter.count}}</h2> -->
+    <button  class="block mx-auto my-4 text-yellow text-10" type="button"  @click="counterStore.increment">useGlobalStore: {{counterStore.count}} {{ counterStore.doubleCount }}</button>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
-import { inject, onMounted } from 'vue'
-// import {useCounterStore} from 'virtual:useGlobalStore'
-import {useGlobalStore} from 'virtual:useGlobalStore'
+import { inject } from 'vue'
+// import { useDepartmentStore,useCounterStore } from 'virtual:useGlobalStore'
+const { useCounterStore } = inject('useGlobalStore')
+const counterStore = useCounterStore()
 
-console.log(2222,
-  inject('useGlobalStore'),
-  useGlobalStore,
-)
 const router = useRouter()
 const goBack = () => {
   router.back()
 }
-onMounted(() => {
-  // console.log(111111,useCounterStore)
-  import('virtual:useGlobalStore').then(res => {
-  console.log('res', res)
-})
-
-})
-
 </script>
--- vite-plugin-pinia-store.js
+++ vite-plugin-pinia-store.js
@@ -1,19 +1,79 @@
+import { init, parse } from 'es-module-lexer'
+import MagicString from 'magic-string'
+import { Parser } from 'acorn'
+
+const ID_FILTER_REG = /\.(mjs|js|ts|vue|jsx|tsx)(\?.*|)$/;
+
 export default function () {
   const virtualModuleId = 'virtual:useGlobalStore'
   const resolvedVirtualModuleId = '\0' + virtualModuleId
 
   return {
     name: 'vite-plugin-pinia-store',
+    enforce: 'pre',
     resolveId(id) {
       if (id === virtualModuleId) {
         return resolvedVirtualModuleId
       }
     },
-    load(id) {
-      if (id === resolvedVirtualModuleId) {
-        // return `export const msg = "from virtual module"`
-        return `import { getCurrentInstance } from 'vue';export const useGlobalStore = getCurrentInstance(); console.log(useGlobalStore)`
-      }
-    },
+
+    // async transform(code, id) {
+
+    //   if (ID_FILTER_REG.test(id)) {
+    //     await init
+
+    //     console.log(code, '/n  xixixix', id)
+    //     const [imports] = parse(code)
+
+    //     let mStr
+    //     imports.forEach(({
+    //       d: dynamic,
+    //       n: dependence,
+    //       ss: statementStart,
+    //       se: statementEnd,
+    //     }) => {
+    //       if (dependence === 'virtual:useGlobalStore') {
+    //         console.log("d, n, ss, se", dynamic, dependence, statementStart, statementEnd)
+    //         console.log('preview code: ', code)
+    //         mStr = mStr || (mStr = new MagicString(code))
+    //         const raw = code.substring(statementStart, statementEnd)
+    //         const newImportStr = transformImports(raw)
+    //         mStr.overwrite(statementStart, statementEnd, newImportStr)
+    //         const ret = {
+    //           code: mStr.toString(),
+    //           map: mStr.generateMap(Object.assign({}, {
+    //             source: id,
+    //             includeContent: true,
+    //             hires: true,
+    //           })),
+    //         }
+    //         console.log(mStr.toString())
+
+    //         return mStr.toString()
+    //       }
+    //     })
+    //   }
+
+    //   return code
+
+    // }
   }
+}
+
+function transformImports(raw) {
+  const ast = Parser.parse(raw, {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  })
+  const specifiers = (ast.body[0])?.specifiers
+  if (!specifiers) {
+    return ''
+  }
+  const code = `const { ${specifiers.map(({ local }) => {
+    return local.name
+  }).join(',')} } = inject('useGlobalStore')`
+  return code
+  return `const { ${specifiers.map(({ local }) => {
+    return local.name
+  }).join(',')} } = inject('useGlobalStore')`
 }
